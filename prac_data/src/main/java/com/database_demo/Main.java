94ce7f48b90e13ae13ec9ee69d2aa008444bca58
Akshayraut33 <akshayraut03052003@gamil.com>
2024-07-01 12:04:56 +0530

    Inital Setup

---
+++ prac_data/src/main/java/com/database_demo/Main.java
@@ -0,0 +1,8 @@
+package com.database_demo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Start Java ! ");
+    }
+}
